275035b62a62d71402f028ac70030ae1e1e984eb
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-26 23:36:28 +0900

    apiルーティング追加

--- api/routes/web.php
+++ api/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/viewer/{id}', function () {
+    return view('welcome');
+});
+
+Route::get('/register', function () {
+    return view('welcome');
+});
