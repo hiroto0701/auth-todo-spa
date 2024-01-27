b77ed92a6f41b4feeb7543355a54976f41e84273
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-27 23:55:52 +0900

    piniaを使用するために一部コードを変更

--- frontend/src/components/TaskRegister.vue
+++ frontend/src/components/TaskRegister.vue
@@ -1,5 +1,61 @@
 <script setup lang="ts">
+import MainHeader from '@/components/MainHeader.vue';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const router = useRouter();
+
+const taskTitle = ref('');
+const priority = ref(1);
+const taskContent = ref('')
+
+const toList = (): void => {
+  router.push({ name: 'home' });
+}
+
+const doRegister = (): void => {
+  const taskTitleValue = taskTitle.value;
+  const priorityValue = priority.value;
+  const taskContentValue = taskContent.value;
+  const isDone = false;
+}
 </script>
 <template>
-  <h2>登録画面作成予定地</h2>
+  <main-header />
+  <main class="w-9/12 h-screen mx-auto my-20">
+    <div class="flex justify-between">
+      <h2 class="text-lg font-serif font-semibold">User名さんのタスク登録</h2>
+      <button class="bg-slate-600 text-white p-3 rounded-lg" @click="toList">戻る</button>
+    </div>
+    <form @submit.prevent="" class="max-w-md mx-auto my-10 bg-white rounded-xl p-10 shadow-lg">
+      <div class="mb-4">
+        <label for="taskName" class="block text-sm font-medium text-gray-600">タスク名</label>
+        <input v-model="taskTitle" type="text" id="taskName" name="taskName"
+              class="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-sky-500">
+      </div>
+
+      <div class="mb-4">
+        <label for="priority" class="block text-sm font-medium text-gray-600">優先度</label>
+        <select v-model="priority" id="priority" name="priority"
+                class="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-sky-500">
+          <option value="1">高</option>
+          <option value="2">普通</option>
+          <option value="3">低</option>
+        </select>
+      </div>
+
+      <div class="mb-4">
+        <label for="taskContent" class="block text-sm font-medium text-gray-600">タスクの内容</label>
+        <textarea v-model="taskContent" id="taskContent" name="taskContent"
+                  class="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-sky-500">
+        </textarea>
+      </div>
+
+      <div class="flex justify-center">
+        <button type="submit" class="bg-sky-500 text-white py-2 px-4 rounded-md hover:bg-sky-600 focus:outline-none focus:border-sky-700 focus:shadow-outline-sky active:bg-sky-800">
+          送信
+        </button>
+      </div>
+    </form>
+  </main>
 </template>
--- frontend/src/stores/todo.ts
+++ frontend/src/stores/todo.ts
@@ -11,35 +11,12 @@ interface Todo {
 
 export const useTodoStore = defineStore('todo', {
   state: () => ({
-    todos: [
-      {
-        "id": 1,
-        "title": "ご飯を作る",
-        "priority": 3,
-        "content": "お昼ご飯を作ります",
-        "isDone": false
-      },
-      {
-        "id": 2,
-        "title": "お風呂に入る",
-        "priority": 2,
-        "content": "お風呂に入ります",
-        "isDone": false
-      },
-      {
-        "id": 9,
-        "title": "寝る",
-        "priority": 3,
-        "content": "寝ます",
-        "isDone": false
-      },
-      {
-        "id": 10,
-        "title": "起きる",
-        "priority": 3,
-        "content": "寝ます",
-        "isDone": true
-      },
-    ] as Todo[],
+    todos: {
+      "id": 1,
+      "title": "ご飯を作る",
+      "priority": 3,
+      "content": "お昼ご飯を作ります",
+      "isDone": false
+    } as Todo,
   }),
 });
