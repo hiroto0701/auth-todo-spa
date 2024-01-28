188f0bc24ac9d68aef37f16f7d1baf1a214ea88f
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-28 23:27:15 +0900

    piniaの切り分けにより

--- frontend/src/components/TaskViewer.vue
+++ frontend/src/components/TaskViewer.vue
@@ -1,12 +1,36 @@
 <script setup lang="ts">
 import MainHeader from '@/components/MainHeader.vue';
 import { useRouter } from 'vue-router';
+import { useTodoFormStore } from '@/stores/todo_form';
+import { computed, onMounted } from 'vue';
 
 const router = useRouter();
+const todoStore = useTodoFormStore();
+
+const todo = computed(() => {
+  return todoStore.todos;
+});
 
 const toList = (): void => {
   router.push({ name: 'home' });
 }
+
+const getPriorityText = (priority: number): string => {
+  switch (priority) {
+    case 1:
+      return "高";
+    case 2:
+      return "普通";
+    case 3:
+      return "低";
+    default:
+      return "";
+  }
+}
+
+onMounted(() => {
+  todoStore.load(router.currentRoute.value.params.id);
+});
 </script>
 
 <template>
@@ -18,9 +42,9 @@ const toList = (): void => {
     </div>
 
     <div class="rounded-lg shadow-md my-8 p-10 cursor-pointer">
-    <h2 class="text-lg font-semibold my-3">タスク名</h2>
-    <p class="my-3">優先度：</p>
-    <p class="text-gray-400 my-3">タスクの内容</p>
-  </div>
+      <h2 class="text-lg font-semibold my-3">{{ todo.title }}</h2>
+      <p class="my-3">優先度：{{ getPriorityText(todo.priority) }}</p>
+      <p class="text-gray-400 my-3">{{ todo.content }}</p>
+    </div>
   </main>
 </template>
